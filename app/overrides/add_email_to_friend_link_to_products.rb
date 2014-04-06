6f0399f66668f98cb5907eb2039efdf824b43611
Ricardo Franco <ricardo.krieg@gmail.com>
2014-04-06 20:54:46 -0300

    update add_email_to_friend_link_to_products.rb

--- app/overrides/add_email_to_friend_link_to_products.rb
+++ app/overrides/add_email_to_friend_link_to_products.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/products/show",
                      :name => "converted_product_description_351026984",
                      :replace_contents => "[data-hook='email-to-friend']",
-                     :text => "<%= link_to Spree.t('email_to_friend.send_to_friend'), email_to_friend_url('product', @product), class: 'mail-button light-blue p-bold', style: 'padding:6px;' %>"
+                     :text => "<%= link_to Spree.t('email_to_friend.send_to_friend'), email_to_friend_url('product', @product), class: 'mail-button light-blue p-bold', style: 'padding:6px;' %>",
                      :disabled => false,
                      :original => "23a81432df67f93cad7563f7b118d6f901b8e42b")
