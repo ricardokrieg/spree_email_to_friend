5188d60e41ed9792a02ba222e8e5ae327eea4be3
Ricardo Franco <ricardo.krieg@gmail.com>
2014-04-06 23:28:31 -0300

    update spree_email_to_friend.js

--- app/assets/javascripts/spree/frontend/spree_email_to_friend.js
+++ app/assets/javascripts/spree/frontend/spree_email_to_friend.js
@@ -1,1 +1,6 @@
 //= require spree/frontend
+
+$('[data-hook="email-to-friend"] a.email-to-friend-link').on('ajax:success', function(e, data, status, xhr) {
+    $('#email-to-friend-modal').html(data);
+    $('#email-to-friend-modal').show();
+});
